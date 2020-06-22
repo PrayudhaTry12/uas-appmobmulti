15dd91626c319df971c81812a4db7202faec8414
PrayudhaTry12 <prayudhatyt@gmail.com>
2020-06-22 08:50:24 +0700

    UAS Aplikasimobilemultiplatform

---
+++ src/providers/service/service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ServiceProvider {
+  API_KEY = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=1a106193416f45c4a24a0ea15dc3a362';
+
+  constructor(public httpClient: HttpClient) {
+    console.log('Hello ServiceProvider Provider');
+  }
+
+  getNews(){
+    return this.httpClient.get(this.API_KEY);
+  }
+
+}
+
+
+
